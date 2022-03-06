0b69e6762248d75d562229a99b52f267194d7280
León <leon.grass@gmx.net>
2022-03-07 00:21:52 +0100

    creating all necessary files

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,3 +1,16 @@
+from uiFiles.GameOfLifeWindow import Ui_MainWindow
+from PySide6 import QtSql, QtGui
+from PySide6.QtWidgets import *
+from PySide6.QtCore import Qt
 
+class GameOfLife(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        
 
-
+app = QApplication()
+MainWindow = GameOfLife()
+MainWindow.setStyleSheet(open('./styleMain.css', encoding='utf-8').read())
+MainWindow.show()
+app.exec()
---
+++ uiFiles/gameBoard.py
@@ -0,0 +1,7 @@
+from PySide6 import QtSql, QtGui
+from PySide6.QtWidgets import *
+from PySide6.QtCore import Qt
+
+class gameBoard(QWidget):
+    def __init__(self):
+        pass
